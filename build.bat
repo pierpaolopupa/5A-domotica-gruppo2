150af528e116d4140ac638f242e43abbc4bb2e3e
federicopiromalli <federico.piromalli@leomail.it>
2025-11-19 08:53:29 +0100

    aggiornamento file build.bat

--- build.bat
+++ build.bat
@@ -2,13 +2,32 @@
 setlocal
 
 set PROJECT_DIR=%~dp0
+set SRC_DIR=%PROJECT_DIR%src\gruppodue
+set BIN_DIR=%PROJECT_DIR%bin
 set CLASSPATH=%PROJECT_DIR%json.jar
 
-if not exist "%PROJECT_DIR%bin" mkdir "%PROJECT_DIR%bin"
+if not exist "%BIN_DIR%" mkdir "%BIN_DIR%"
 
-javac -cp "%CLASSPATH%" -d "%PROJECT_DIR%bin" "%PROJECT_DIR%gruppodue\*.java"
+REM Creo un file che contiene la lista di tutti i .java
+dir /b /s "%SRC_DIR%\*.java" > "%PROJECT_DIR%sources.txt"
 
-start cmd /k "cd /d %PROJECT_DIR%bin && java -cp .;%CLASSPATH% gruppodue.Server"
-start cmd /k "cd /d %PROJECT_DIR%bin && java -cp .;%CLASSPATH% gruppodue.Client"
+REM Compilo in bin
+javac -cp "%CLASSPATH%" -d "%BIN_DIR%" @"%PROJECT_DIR%sources.txt"
+if errorlevel 1 (
+    del "%PROJECT_DIR%sources.txt"
+    pause
+    exit /b
+)
+
+del "%PROJECT_DIR%sources.txt"
+
+REM Avvio server
+start "" cmd /k "cd /d %BIN_DIR% && java -cp .;%CLASSPATH% gruppodue.Server"
+
+REM Aspetto un secondo per far partire il server
+timeout /t 1 >nul
+
+REM Avvio client
+start "" cmd /k "cd /d %BIN_DIR% && java -cp .;%CLASSPATH% gruppodue.Client"
 
 endlocal
