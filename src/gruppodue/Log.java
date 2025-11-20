7d40be1be43d40a59879105095a68fd4b10f72b3
federicopiromalli <federico.piromalli@leomail.it>
2025-11-20 18:12:15 +0100

    cambiamenti minori

--- src/gruppodue/Log.java
+++ src/gruppodue/Log.java
@@ -5,13 +5,13 @@ import java.io.IOException;
 
 public class Log {
   /**
-   * @param type Il tipo di log (INFO, AVVISO, ALLARME).
+   * @param type Il tipo di log (INFO, AVVISO, ALLARME, RICHIESTA).
    * @param message Il messaggio da loggare.
    * @param stream La fonte a cui inviare il log.
    * @throws IOException
    */
   public void logMessage(String type, String message, DataOutputStream stream) throws IOException {
-    final String log = "(" + type + "): " + message + ".\n";
+    final String log = "\n(" + type + "): " + message + ".\n\n";
     if (stream != null)
       stream.writeBytes(log);
     else
