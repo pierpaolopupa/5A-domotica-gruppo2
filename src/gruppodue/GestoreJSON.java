67985038820a9581361f9eab091b3826d6982a17
federicopiromalli <federico.piromalli@leomail.it>
2025-11-20 18:37:34 +0100

    tolta funzionalita' id che faceva crashare il client

--- src/gruppodue/GestoreJSON.java
+++ src/gruppodue/GestoreJSON.java
@@ -6,14 +6,6 @@
 
   public class GestoreJSON {
     private ArrayList<JSONObject> richiesteJson = new ArrayList<JSONObject>();
-    private int creaNuovoId() {
-      if (this.richiesteJson.isEmpty())
-        return 0;
-      return Integer.parseInt(
-        this.richiesteJson.get(
-          this.richiesteJson.size() - 1).getString("ID")
-      ) + 1; 
-    }
     /**
      * @param tipo Chi sta inviando? (contatto, movimento, temperatura).
      * @param valoreDouble Il valore per la temperatura.
@@ -24,7 +16,6 @@
      */
     public String generaDati(String tipo, String valoreDouble, String valoreBool, String zona, String ora) {
       JSONObject json = new JSONObject();
-      json.put("ID", this.creaNuovoId());
       json.put("tipo", tipo.toLowerCase());
       switch (tipo.toLowerCase()) {
         case "exit" -> {}
