05a1837a131c791915ecbfd807805eb347f1c21a
federicopiromalli <federico.piromalli@leomail.it>
2025-11-21 20:40:26 +0100

    fp: gestore json senza funz. id

---
+++ src/gruppodue/JsonHandler.java
@@ -0,0 +1,27 @@
+package gruppodue;
+
+import java.util.ArrayList;
+import org.json.JSONObject;
+
+public class JsonHandler {
+  private ArrayList<JSONObject> cronologiaJson;
+  public JsonHandler() {
+    this.cronologiaJson = new ArrayList<JSONObject>();
+  }
+  /**
+   * @param tipo Chi ha inviato i dati.
+   * @param valore Che valore hanno i dati (decimale se temperatura).
+   * @param zona In che zona sono stati rilevati (nessuna se temperatura).
+   * @param ora A che ora sono stati rilevati (nessuna se temperatura).
+   * @return JSON con i dati inviati.
+   */
+  public String creaDatiJson(final String valore, final String zona, final String ora) {
+    // Invio un JSON con struttura uguale per tutti per render il codice piu' leggibile.
+    JSONObject jo = new JSONObject();
+    jo.put("valore", valore);
+    jo.put("zona", zona);
+    jo.put("ora", ora);
+    this.cronologiaJson.add(jo);
+    return jo.toString();
+  }
+}
