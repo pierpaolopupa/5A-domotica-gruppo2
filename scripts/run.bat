25f9c5910fb00ed9f94e6917f30832f51a67a303
federicopiromalli <federico.piromalli@leomail.it>
2025-11-21 17:29:45 +0100

    fp: cambiamenti minori allo script

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,5 +1,11 @@
 @echo off
 
+:: Termina eventuali processi che occupano la porta 1234
+for /f "tokens=5" %%i in ('netstat -ano ^| findstr :1234') do taskkill /f /pid %%i
+
+:: Termina eventuali processi di gruppodue.Server e gruppodue.Client
+taskkill /f /im java.exe /t
+
 :: Pulisce la finestra del terminale
 cls
 
