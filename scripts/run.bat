e37076bbbedf9bbaf4db97d3d70dd3bf5e9f0ad5
federicopiromalli <federico.piromalli@leomail.it>
2025-11-24 21:33:59 +0100

    Update run.bat

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,26 +1,21 @@
 @echo off
-REM Libera la porta usata dal Server (termina processi che usano la porta 1234)
-for /f "tokens=5" %%a in ('netstat -ano ^| findstr :1234') do (
+
+set PORT=1234
+set LIB_PATH=lib\json-java.jar
+set SRC_PATH=src\gruppodue
+set BIN_PATH=bin
+
+for /f "tokens=5" %%a in ('netstat -ano ^| findstr :%PORT%') do (
     taskkill /PID %%a /F >nul 2>&1
 )
 
-REM Termina eventuali processi di gruppodue.Server e gruppodue.Client
-taskkill /F /IM java.exe >nul 2>&1
-
-REM Pulisce il terminale
 cls
 
-REM Percorso della libreria JSON
-set LIB_PATH=lib\json-java.jar
+javac -d "%BIN_PATH%" -cp "%LIB_PATH%" "%SRC_PATH%\*.java"
+if errorlevel 1 exit /b
 
-REM Compila i file .java nella cartella src\gruppodue\ includendo la libreria
-javac -d bin -cp "%LIB_PATH%" src\gruppodue\*.java
+start /b "" java -cp "%LIB_PATH%;%BIN_PATH%" gruppodue.Server
 
-REM Esegui il file Server.java nel terminale corrente includendo la libreria
-start "Server" cmd /c java -cp "%LIB_PATH%;bin" gruppodue.Server
-
-REM Attendi un secondo per dare tempo al Server di partire
 timeout /t 1 >nul
 
-REM Esegui il file Client.java in un altro terminale includendo la libreria
-start "Client" cmd /c java -cp "%LIB_PATH%;bin" gruppodue.Client
+start "" cmd /c java -cp "%LIB_PATH%;%BIN_PATH%" gruppodue.Client
